5608578ff703a6a4d7f59e61ba9df5b520f55d52
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-04-19 18:33:29 -0300

    15

---
+++ Lista03/src/br/edu/up/exercicios/Exercicio_15.java
@@ -0,0 +1,10 @@
+package br.edu.up.exercicios;
+import br.edu.up.modelos.ConcessionariaCarango;
+public class Exercicio_15 {
+    public static void executar(){
+        ConcessionariaCarango concessionariaCarango = new ConcessionariaCarango();
+        concessionariaCarango.calcularDescontoEValorPago();
+        concessionariaCarango.mostrarTotais();
+    }
+    
+}
